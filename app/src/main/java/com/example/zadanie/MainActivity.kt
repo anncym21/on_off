3f0b0e09e0ee56eb98c3c8c78a4a044c43bb7369
student <cymanowska.anna@wp.pl>
2021-11-05 15:40:26 +0100

    Dodanie przycisków i obrazków i zmiany w MainActivity.kt

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import android.widget.ToggleButton
 import com.example.zadanie.databinding.ActivityMainBinding
 
 
@@ -22,6 +25,30 @@ class MainActivity : AppCompatActivity() {
         binding.toggleButtonID.setOnCheckedChangeListener { buttonView, isChecked ->
             val msg = "Toggle Button is " + if (isChecked) "ON" else "OFF"
             Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+            val toggle: ToggleButton = findViewById(R.id.red)
+            toggle.setOnClickListener(){
+                if(isChecked){
+                    ImageView.INVISIBLE
+                }else {
+                    ImageView.VISIBLE
+                }
+            }
+            val toggle1: ToggleButton = findViewById(R.id.blue)
+            toggle.setOnClickListener(){
+                if(isChecked){
+                    ImageView.INVISIBLE
+                }else{
+                    ImageView.VISIBLE
+                }
+            }
+            val toggle2: ToggleButton = findViewById(R.id.green)
+            toggle.setOnClickListener(){
+                if(isChecked){
+                    ImageView.INVISIBLE
+                }else{
+                    ImageView.VISIBLE
+                }
+            }
         }
     }
 }
